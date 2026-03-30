bbeed31891bb9fc09b18b6a7665e3356e60e3ddd
angelinapellat-ux <angelina.pellat@gmail.com>
2026-03-30 19:33:21 +0200

    feat : add job05

---
+++ Jour1/job05.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class jour01 {
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Saisis le premier nombre : ");
+        int a = clavier.nextInt();
+
+        System.out.print("Saisis le deuxième nombre : ");
+        int b = clavier.nextInt();
+
+        System.out.print("Saisis le troisième nombre : ");
+        int c = clavier.nextInt();
+
+        int max = Math.max(a, Math.max(b, c));
+
+        System.out.println("Le maximum des trois nombres est : " + max);
+
+        clavier.close();
+    }
+}
