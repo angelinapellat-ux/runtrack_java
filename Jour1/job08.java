b7427930754c93826b8831b50359f6a716dde1e1
angelinapellat-ux <angelina.pellat@gmail.com>
2026-03-30 20:01:00 +0200

    feat : add job08

---
+++ Jour1/job08.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class jour01 {
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Saisis un entier n : ");
+        int n = clavier.nextInt();
+
+        int somme = 0;
+
+        for (int i = 1; i <= n; i++) {
+            somme += i * i * i;
+        }
+
+        System.out.println("La somme des " + n + " premiers cubes est : " + somme);
+
+        clavier.close();
+    }
+}
