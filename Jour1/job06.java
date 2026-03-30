df0cf9b3288d1b6db38ea546dcec8450d389665d
angelinapellat-ux <angelina.pellat@gmail.com>
2026-03-30 19:37:49 +0200

    feat : add job06

---
+++ Jour1/job06.java
@@ -0,0 +1,20 @@
+public class jour01 {
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+
+        int somme = 0;
+        int n;
+
+        System.out.println("Saisis cinq entiers :");
+
+        for (int i = 0; i < 5; i++) {
+            n = clavier.nextInt();
+            somme += n;
+        }
+
+        System.out.println("La moyenne est : " + (somme / 5.0));
+
+        clavier.close();
+    }
+}
