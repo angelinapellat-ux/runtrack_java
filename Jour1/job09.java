f655d279b9a56f8d4037cf6652877e4d74d7d107
angelinapellat-ux <angelina.pellat@gmail.com>
2026-03-30 20:04:41 +0200

    feat : add job09

---
+++ Jour1/job09.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class jour01 {
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Saisis ton âge : ");
+        int age = clavier.nextInt();
+
+        if (age < 18) {
+            System.out.println("Tu es mineur.");
+        } else {
+            System.out.println("Tu es majeur.");
+        }
+
+        clavier.close();
+    }
+}
