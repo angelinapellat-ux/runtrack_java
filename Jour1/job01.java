4b0ec2a252608967ed09fa803b7b9fb8a454bf62
angelinapellat-ux <angelina.pellat@gmail.com>
2026-03-30 18:05:18 +0200

    feat : add job01

---
+++ Jour1/job01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class jour01 {
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Tape quelque chose : ");
+        String saisie = clavier.nextLine();
+
+        System.out.println("Tu as tapé : " + saisie);
+
+        clavier.close();
+    }
+}
