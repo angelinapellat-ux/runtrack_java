f135a282f06f22d43c1aab9dc07a021a3bd17ef2
angelinapellat-ux <angelina.pellat@gmail.com>
2026-04-01 12:56:06 +0200

    feat : add job0

---
+++ Jour2/job0.java
@@ -0,0 +1,15 @@
+public class job0 {
+
+    // Attributs publics
+    public String nom;
+    public String prenom;
+
+    // Attributs protégés
+    protected String dateNaissance;
+    protected String lieuNaissance;
+
+    // Attributs privés
+    private String adresse;
+    private String telephone;
+
+}
